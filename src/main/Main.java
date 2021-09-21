62eb6f242ca4fe248cbe15de2016b78817c1a594
juapage <juanantonio.pages@alumnos.uva.es>
2021-09-21 18:00:48 +0200

    javaProject creado

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package src.main;
+/*Practica1 de EDA realizada por Juan Antonio Pages Lopez */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String fileName = "";
+
+        System.out.println("Nombre del archivo: ");
+
+        Scanner scanner = new Scanner(System.in);
+        fileName = scanner.nextLine();
+        
+        
+        
+
+    }
+}
